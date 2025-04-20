e24d5aa08ed5ca386e3403b4cf21170aa79b27d9
hyeonghan <galaxykhh@gmail.com>
2025-04-20 21:53:40 +0900

    first commit

---
+++ test/immutable_list_test.dart
@@ -0,0 +1,16 @@
+import 'package:immutable_list/immutable_list.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
